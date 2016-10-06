daa5cb3b8b9d5ff99a50b82c46f8bad0e4d8b604
Biencis Rubiera <biencis@gmail.com>
2016-10-06 05:47:58 +0000

    Created task_03 and then tested it with testtask03

---
+++ task_03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Practicing with tuples"""
+
+import data
+
+DIRECTIONS = data.DIRECTIONS
+
+DIRECTIONS = DIRECTIONS[0:3] + ('West',)
---
+++ testtask03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Testing tuple change done in task_03"""
+
+import task_03
+
+print task_03.DIRECTIONS
