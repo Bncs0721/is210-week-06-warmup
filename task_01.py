bf8ad2d3bc7209aab994720229fdc8177920a100
Biencis Rubiera <biencis@gmail.com>
2016-10-06 04:20:03 +0000

    Created task_01 which contains a tuple and a list

---
+++ task_01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Defining a list (ELEMENTS) and a tuple (OPERATIONS_ORDER)"""
+
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron',
+            'Carbon')
+
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division', 
+                    'Addition', 'Subtraction']
